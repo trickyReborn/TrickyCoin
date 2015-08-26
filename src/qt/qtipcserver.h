40e11cd02e29fdd15dd0c6de44d5aa5896c3b189
trickyReborn <trickyfaucet@gmail.com>
2015-08-26 07:32:22 -0700

    So... Here we go!
    ... at least.

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define TrickyCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "TrickyCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
