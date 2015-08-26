a0331279af30818a0019128d43181a7c82a51b8d
trickyReborn <trickyfaucet@gmail.com>
2015-08-26 08:31:05 -0700

    Adding DNS seeds
    Added trickycoin.com DNS seeds

--- src/net.cpp
+++ src/net.cpp
@@ -1139,9 +1139,9 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    	{"trickycoin.com", "52.11.105.205"},
-      
-    	
+    	{"trickycoin.com", "node.trickycoin.com"},
+    	{"trickycoin.com", "masternode.trickycoin.com"},
+    	{"trickycoin.com", "dnsseed.trickycoin.com"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
